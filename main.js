356d1685df5199858e62a48db867475551dd1a11
vedkpatel201210 <98235037+vedkpatel201210@users.noreply.github.com>
2023-10-12 20:30:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+video = createCapture(VIDEO);
+video.size(550,500);
+canvas = createCanvas(550,500);
+canvas.position(560,150);
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose' , gotPoses)
+}
+
+function draw(){
+    background:"aliceblue"; 
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log("results");
+    }
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
